9e7ebc86903d0e0c8dafadfae4700b67993aa45c
IT21826740 <IT21826740@my.sliit.lk>
2025-05-27 09:38:27 +0530

    first commit

---
+++ src/test/java/lk/dilini/spring_security_jwt_authentication/SpringSecurityJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dilini.spring_security_jwt_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
